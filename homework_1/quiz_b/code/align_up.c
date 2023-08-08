2470b9f90894e466835d78d17db5c24fffe3da9d
Gavin_Wu <gavin_wu@phison.com>
2023-08-08 12:46:50 +0800

    Fill the blank in quiz_b
        Issue ID: Empty
        Risk: low/high
        Description: Nothing Special.

--- homework_1/quiz_b/code/align_up.c
+++ homework_1/quiz_b/code/align_up.c
@@ -1,11 +1,20 @@
 #include <stdint.h>
 
+/*
+ *    2的冪在2進位中 只會有1個BIT的1
+ *    2 = 0x0010b  -> 2-1 = 0x0001b
+ *    4 = 0x0100b  -> 4-2 = 0x0011b
+ *    8 = 0x1000b  -> 8-2 = 0x0111b
+ *
+ *    和自己的-1 做&運算可以檢查出是不是2的冪
+ *
+ *    如果是2的冪, align只要把後面的部份去掉就好
+ */
 static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
     uintptr_t mask = alignment - 1;
-    if ((alignment & mask) == 0)
-    { /* power of two? */
-        return MMMM;
+    if ((alignment & mask) == 0){ /* power of two? */
+        return sz & (~mask);
     }
     return (((sz + mask) / alignment) * alignment);
 }
